f86333136b87a98fccbdca84eb3e42037573820a
Sam Tay <sam.chong.tay@gmail.com>
2016-03-09 23:59:05 -0500

    Move configuration to json file

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-var gitLabel = require('git-label');
-
-// Configuration
-var REPO = "github-label-manager", // repo to modify
-    TOKEN = "", // add personal github token
-    OWNER = "SamTay", // user or organization
-    PACKAGE_ADD = 'packages/samtay/*.json', // glob for new labels to add
-    PACKAGE_REMOVE = 'packages/default.json'; // glob for labels to remove
+var gitLabel = require("git-label");
+var config = require("./config");
 
 // Add and remove packages
-gitLabel.find(PACKAGE_ADD).then(add).catch(console.log);
-gitLabel.find(PACKAGE_REMOVE).then(remove).catch(console.log);
+gitLabel.find(config.packages_to_add).then(add).catch(console.log);
+gitLabel.find(config.packages_to_remove).then(remove).catch(console.log);
 
-var config = {
+// Wrap gitLabel methods
+var gitConfig = {
   api   : "https://api.github.com",
-  repo  : OWNER + "/" + REPO,
-  token : TOKEN
+  repo  : config.owner + "/" + config.repository,
+  token : config.token
 };
 function add(labels) {
-  gitLabel.add(config, labels)
+  gitLabel.add(gitConfig, labels)
     .then(console.log)
     .catch(console.log);
 }
 function remove(labels) {
-  gitLabel.remove(config, labels)
+  gitLabel.remove(gitConfig, labels)
     .then(console.log)
     .catch(console.log);
 }
